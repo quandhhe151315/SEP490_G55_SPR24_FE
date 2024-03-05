16f787a2a53346bd8a8e990631717cb46430cb47
PhanTuanLinhHE140842 <linhpthe140842@fpt.edu.vn>
2024-03-05 16:45:29 +0700

    UPDATE Components Appbar, Snackbar

--- kitchendelights/src/components/JWT/GetInformationJWT.jsx
+++ kitchendelights/src/components/JWT/GetInformationJWT.jsx
@@ -7,15 +7,16 @@ function GetInformationJWT(props) {
 
     const decodeJWT = () => {
         const token = Cookies.get('jwt');
-        const decoded = jwtDecode(token);
-        if (setId) setId(decoded.id);
-        Cookies.set('userId', decoded.id);
-        // Cookies.set('username', decoded.username);
-    if (setEmail) setEmail(decoded.email);
-    if (setRole) setRole(decoded.role);
-    if (setName) setName(decoded.name);
-    if (setAvatar) setAvatar(decoded.avatar);
-    console.log(decoded.email)
+        if(token != null){
+          console.log("Token not null");
+          const decoded = jwtDecode(token);
+          if (setId) setId(decoded.id);
+          Cookies.set('userId', decoded.id, { expires: 7 });
+          if (setEmail) setEmail(decoded.email);
+          if (setRole) setRole(decoded.role);
+          if (setName) setName(decoded.name);
+          if (setAvatar) setAvatar(decoded.avatar);
+        }
       }
     
       useEffect(() => {
