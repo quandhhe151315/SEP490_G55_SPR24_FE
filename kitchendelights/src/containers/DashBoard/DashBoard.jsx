82674afab3c2526a4594de91e9f42180aad59e75
nhan151206 <nhanpthe151206@fpt.edu.vn>
2024-04-01 04:01:28 +0700

    chart

--- kitchendelights/src/containers/DashBoard/DashBoard.jsx
+++ kitchendelights/src/containers/DashBoard/DashBoard.jsx
@@ -4,6 +4,7 @@ import Paper from '@mui/material/Paper';
 import DashboardMenu from "../../components/Dashboard/Menu/DashboardMenu";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
+import { BarChart } from '@mui/x-charts/BarChart';
 
 
 function DashBoard() {
@@ -18,12 +19,12 @@ function DashBoard() {
                 <Paper elevation={2} sx={{ borderRadius: '15px', border: '1px solid #bfb8b8', width: '1350px', height: '700px', backgroundColor: '#FFFFFF' }}>
                     <Grid container>
                         <Grid item xs={6}>
-                            {/* <BarChart
+                            <BarChart
                                 xAxis={[{ scaleType: 'band', data: ['group A', 'group B', 'group C'] }]}
                                 series={[{ data: [4, 3, 5] }, { data: [1, 6, 3] }, { data: [2, 5, 6] }]}
                                 width={500}
                                 height={300}
-                            /> */}
+                            />
                         </Grid>
                     </Grid>
                 </Paper>
