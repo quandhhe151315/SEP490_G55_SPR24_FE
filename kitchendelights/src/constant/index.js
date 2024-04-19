1b026ebe07cd927796abca63d97075deb2ef2d16
hieupmhe141021 <hieupmhe141021@fpt.edu.vn>
2024-04-19 11:15:38 +0700

    fix bug

--- kitchendelights/src/constant/index.js
+++ kitchendelights/src/constant/index.js
@@ -1,4 +1,4 @@
 // export const BASE_URL = "http://localhost:5050/api";
 
-//export const BASE_URL = "http://localhost:5050/api";
-export const BASE_URL = "http://localhost:4200/api";
+export const BASE_URL = "http://localhost:5050/api";
+// export const BASE_URL = "http://localhost:4200/api";
