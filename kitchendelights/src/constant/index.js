190ba0b544e84b8f9fa25a9aa0684db6209bd6f0
nhan151206 <nhanpthe151206@fpt.edu.vn>
2024-04-23 13:36:27 +0700

    Update index.js

--- kitchendelights/src/constant/index.js
+++ kitchendelights/src/constant/index.js
@@ -1,9 +1,4 @@
 // export const BASE_URL = "http://localhost:5050/api";
 
-<<<<<<< Updated upstream
 //export const BASE_URL = "http://localhost:5050/api";
  export const BASE_URL = "http://localhost:4200/api";
-=======
-// export const BASE_URL = "http://localhost:5050/api";
-export const BASE_URL = "http://localhost:4200/api";
->>>>>>> Stashed changes
